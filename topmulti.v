92ed1d4cdfc16a1f408b9657c75379cffc6fdefe
DhruvkBhatt <bdhruv32@gmail.com>
2020-03-16 22:28:37 +0530

    top of multi cycle

---
+++ topmulti.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    12:36:25 03/16/2020 
+// Design Name: 
+// Module Name:    topmulti 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module topmulti(input         clk, reset, 
+                output [31:0] writedata, adr, 
+                output        memwrite);
+
+  wire [31:0] readdata;
+  
+  // instantiate processor and memory
+  mips mips(clk, reset, adr, writedata, memwrite, readdata);
+  mem mem(clk, memwrite, adr, writedata, readdata);
+
+endmodule
+
